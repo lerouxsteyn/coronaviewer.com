925d0c41a4b9eb9169395914a59f55475ae86866
Lex <leroux@artificial.studio>
2020-03-26 14:52:25 +0200

    Do production build locally

---
+++ build/precache-manifest.33fcb18ca35f4021e3c12fde71c63fdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59eaaba5d94ba44d3f43e47dfe0b36f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "357ae101fab51ffb3cb2",
+    "url": "/static/css/main.64147c1f.chunk.css"
+  },
+  {
+    "revision": "31d6f5ceb49cd05a93de",
+    "url": "/static/js/2.84d40093.chunk.js"
+  },
+  {
+    "revision": "8405fc7db03aa55859ceaaa8183fa901",
+    "url": "/static/js/2.84d40093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "357ae101fab51ffb3cb2",
+    "url": "/static/js/main.db6f7bbf.chunk.js"
+  },
+  {
+    "revision": "a8d3bc25cbeac7db4a84",
+    "url": "/static/js/runtime-main.87bba8cd.js"
+  }
+]);
