7358b493462fd68a53fcbf0ba34ad1f499663aa7
Le Roux <leroux@artificialstudio.com>
2020-11-19 00:00:47 +0000

    Data

---
+++ build/precache-manifest.a52e8beb5c3fe673aedcdffb28998a54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b4a141ded006b079585fc1e350cfa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8e1e87d65c326a82f84",
+    "url": "/static/css/main.6d3a7d4c.chunk.css"
+  },
+  {
+    "revision": "db17a2aa6623753b603f",
+    "url": "/static/js/2.3ecf7dc6.chunk.js"
+  },
+  {
+    "revision": "8405fc7db03aa55859ceaaa8183fa901",
+    "url": "/static/js/2.3ecf7dc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e1e87d65c326a82f84",
+    "url": "/static/js/main.324fb5f7.chunk.js"
+  },
+  {
+    "revision": "a8d3bc25cbeac7db4a84",
+    "url": "/static/js/runtime-main.87bba8cd.js"
+  }
+]);
