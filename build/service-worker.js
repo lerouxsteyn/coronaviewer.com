1a560971649302ef0af5456194f2bf5117ebb72a
Le Roux <leroux@artificialstudio.com>
2021-09-13 00:00:43 +0000

    Data

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.292804570a2d65b87b9a045013061fab.js"
+  "/precache-manifest.092250c0c53fdea910357064e6b09f02.js"
 );
 
 self.addEventListener('message', (event) => {
